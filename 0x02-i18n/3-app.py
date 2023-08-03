2406771a87a710bc448990777edd4673420f9ab0
Abd-al-qudus <Abd-al-qudus>
2023-08-03 23:33:36 +0000

    compiled translations

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""a flask app with a / route to return hello world,
+    with configured available language using a config
+    class"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """babel configuration for default language and time"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+@app.route('/')
+def home():
+    """return a simple welcome string"""
+    return render_template('1-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """get user locale and translate"""
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+app.config.from_object(Config)
